d05eb95b0c6dc22312aeca58cde93d538a54a875
Juan Pablo García Villa <juanpablogarciavilla2005@gmail.com>
2023-03-02 17:31:14 -0500

    Primer commit

---
+++ angulos_triangulo_seccion4/angulos.js
@@ -0,0 +1,8 @@
+let angulo1= parseFloat(prompt("Ingrese la medida del angulo numero 1"))
+let angulo2= parseFloat(prompt("Ingrese la medida del angulo numero 2"))
+let angulo3= parseFloat(prompt("Ingrese la medida del angulo numero 3"))
+if (angulo1 + angulo2 + angulo3 ===180){
+    console.log("Estos ángulos si conforman un triángulo")
+}else{
+    console.log("Estos ángulos no conforman un triángulo")
+}
