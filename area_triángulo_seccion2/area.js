c486943df1486e64e0ddf320407306d0160d9b58
Juan Pablo García Villa <juanpablogarciavilla2005@gmail.com>
2023-03-02 16:37:59 -0500

    Primer commit

---
+++ area_triángulo_seccion2/area.js
@@ -0,0 +1,4 @@
+let base = parseInt(prompt("Ingrese la base del triángulo"))
+let altura= parseInt(prompt("Ingrese la altura del triángulo"))
+let area= base * altura
+console.log("El area del triángulo es:", area)
---
+++ porcentaje_cantidad_seccion2/porcentaje.js
@@ -0,0 +1,5 @@
+let cantidad= parseFloat(prompt("Ingrese la cantidad"))
+let porcentaje= parseFloat(prompt("Ingrese el procentaje"))
+let porcentajeSalida= porcentaje / 100
+let cantidadFinal= cantidad * porcentajeSalida
+console.log("El porcentaje de la cantidad dada es:", cantidadFinal)
